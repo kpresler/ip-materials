ee5046e5a048a6941d8e2b0c64face5c72057f0c
Kai Presler-Marshall <kai@cs.jhu.edu>
2026-01-22 11:31:09 -0500

    minor formatting fix!

--- 01 (Jan)/23/printf_ex.c
+++ 01 (Jan)/23/printf_ex.c
@@ -2,17 +2,15 @@
 
 int main(void){
     int first = 10;
-	
-	float second = 20;
-	
-	
-	printf("first, printed as an int: %d\n", first);
-	printf("first, printed as a float: %f\n", first);
-	
-	printf("second, printed as a float: %f\n", second);
-	printf("second, printed as an int: %d\n", second);
-	
-	
-	return 0;
+    float second = 20;
+
+    printf("first, printed as an int: %d\n", first);
+    printf("first, printed as a float: %f\n", first);
+
+    printf("second, printed as a float: %f\n", second);
+    printf("second, printed as an int: %d\n", second);
+
+
+    return 0;
 
 }
