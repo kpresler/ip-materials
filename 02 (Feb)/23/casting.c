924948c583f4f991ade803bf4afc412b8ea11e79
Kai Presler-Marshall <kai@presler-marshall.org>
2026-02-23 12:28:41 -0500

    fix example

--- 02 (Feb)/23/casting.c
+++ 02 (Feb)/23/casting.c
@@ -14,6 +14,24 @@ int sq(int num) {
 
 int main (void) {
 
+    // suppose we've got the following:
+
+    int i1 = 10;
+    double d1 = 13.7;
+    double res = i1 + d1;
+
+    // clearly, C can't just take the 10 and the 13.7 and add them as-is
+    // one is an int, the other is a double -- not only are they different sizes
+    // (the int is 32 bits, the double 64), _but_ the representations (2s complement
+    // vs IEEE-754 64b) are totally different.  So, the computer must _convert_ one
+    // type into that of the other, so that the arithmetic can proceed
+    // in this case, the int is _promoted_ into a double (how much of a pay raise comes with that?)
+    // and then the arithmetic can proceed
+
+
+    // type conversion comes into play in many other scenarios, too
+
+
     int a = 2048;
 
     // automatic type conversion -- in this case, narrowing
@@ -25,13 +43,13 @@ int main (void) {
 
 
     /// why ?
-    // 2048 == 1000 0000 0000
+    // 2048 == 0000 1000 0000 0000
     // when we cast to a char, only the part that fits
     // (the last 1 byte / 8 bits) are kept
     // everything else -- gone
 
     // let's go see another example
-    // 1135 == 0100 0110 1111
+    // 1135 == 0000 0100 0110 1111
 
     // cast this to char, should keep 0110 1111 (== 111_10)
 
@@ -45,7 +63,7 @@ int main (void) {
     // more automatic conversion -- promotion
 
     c = 37;
-    a = c;
+    a = c;    // the char is promoted to an int
     printf("a=%d, c=%d\n", a, c);
 
 
@@ -72,6 +90,10 @@ int main (void) {
     // just for fun, let's go look at the bits of a float
     float f = 20.3f;
 
+    // this hack is done to get the bits of a float into an int
+    // a typecast will be cleverer and convert the float to an int,
+    // keeping the value, but rearranging the bits to do so.  We want
+    // the bits exactly as-is, ignoring the value
     int i = *(int*) &f;
 
     printBits(i);
